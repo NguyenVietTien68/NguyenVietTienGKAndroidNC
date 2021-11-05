e0c232b22fc542be01bee6551baa5da7cb97349f
LAPTOP <tiencuongkhoa71@gmail.com>
2021-11-05 13:10:51 +0700

    Xong câu 2: Xây dựng thiết kế như trên figma

---
+++ app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt/TrangChinh.java
@@ -0,0 +1,14 @@
+package com.example.a33_18078881_nguyenviettien_dhktpm14ctt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangChinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chinh);
+    }
+}
