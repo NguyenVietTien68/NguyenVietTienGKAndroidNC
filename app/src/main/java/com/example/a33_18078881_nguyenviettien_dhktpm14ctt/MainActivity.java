1fe99c2b82c6766f1c0abeb7463063621d777326
LAPTOP <tiencuongkhoa71@gmail.com>
2021-11-05 13:29:44 +0700

    Xong câu 3: Chức năng login với google

--- app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt/MainActivity.java
+++ app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt/MainActivity.java
@@ -2,13 +2,91 @@ package com.example.a33_18078881_nguyenviettien_dhktpm14ctt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDangNhap;
+    SignInButton signin;
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Chức năng đang được xây dựng", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+
+
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            startActivity(new Intent(MainActivity.this, TrangChinh.class));
+
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personEmail = acct.getEmail();
+                Toast.makeText(this, "Dang nhap email: "+personEmail, Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (ApiException e) {
+            Log.d("Error", e.toString());
+        }
     }
 }
--- app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt/TrangChinh.java
+++ app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt/TrangChinh.java
@@ -1,14 +1,72 @@
 package com.example.a33_18078881_nguyenviettien_dhktpm14ctt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class TrangChinh extends AppCompatActivity {
 
+    ImageView imgEmail;
+    TextView tvTen;
+    Button btnSignOut;
+    GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chinh);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        imgEmail = findViewById(R.id.imgEmail);
+        tvTen = findViewById(R.id.textView4);
+        btnSignOut = findViewById(R.id.button);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button:
+                        signOut();
+                        break;
+                }
+            }
+        });
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            tvTen.setText(personName);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(imgEmail);
+        }
+    }
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(TrangChinh.this, "Đăng xuất thành công", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
     }
 }
